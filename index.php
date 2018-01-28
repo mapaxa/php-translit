eddcc03496ce2af24e7edc2fb6d46d3366808318
mapaxa <alexeybespalov86@gmail.com>
2018-01-28 19:07:24 +0200

    basic realization of script

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+<?php //phpinfo(); ?>
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+  <input type="text" name="word">
+  <input type="submit" value="нажать" name="submit">
+</form>
+
+<?php 
+function rus2translit($string) {
+    $converter = array(
+        'а' => 'a',   'б' => 'b',   'в' => 'v',
+        'г' => 'g',   'д' => 'd',   'е' => 'e',
+        'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
+        'и' => 'i',   'й' => 'y',   'к' => 'k',
+        'л' => 'l',   'м' => 'm',   'н' => 'n',
+        'о' => 'o',   'п' => 'p',   'р' => 'r',
+        'с' => 's',   'т' => 't',   'у' => 'u',
+        'ф' => 'f',   'х' => 'h',   'ц' => 'c',
+        'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch',
+        'ь' => '',    'ы' => 'y',   'ъ' => '',
+        'э' => 'e',   'ю' => 'yu',  'я' => 'ya',
+        
+        'А' => 'A',   'Б' => 'B',   'В' => 'V',
+        'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
+        'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
+        'И' => 'I',   'Й' => 'Y',   'К' => 'K',
+        'Л' => 'L',   'М' => 'M',   'Н' => 'N',
+        'О' => 'O',   'П' => 'P',   'Р' => 'R',
+        'С' => 'S',   'Т' => 'T',   'У' => 'U',
+        'Ф' => 'F',   'Х' => 'H',   'Ц' => 'C',
+        'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Sch',
+        'Ь' => '',    'Ы' => 'Y',   'Ъ' => '',
+        'Э' => 'E',   'Ю' => 'Yu',  'Я' => 'Ya',
+        ' ' => '-',
+    );
+    
+    return strtr($string, $converter);
+
+}
+
+if(isset($_POST['submit'])) {
+  if(isset($_POST['word']) && !empty($_POST['word'])) {
+    $word = $_POST['word'];
+  }
+$word = trim(strtolower(rus2translit($word)));
+
+$word = preg_replace("/[^a-zA-ZА-Яа-я0-9\s]/", '-', $word);
+
+echo $word;
+
+}
+
+
+
+ ?>
+  
+</body>
+</html>
+
